406f2d4d7c2b7c654c59e7c8eb1eccdc2773997c
Mambix Ltd <ledi.mambix@gmail.com>
2018-12-29 15:19:36 +0000

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,6 @@
+echo "mysql_host = $HOST" > xbt_tracker.conf
+echo "mysql_user = $USER" >> xbt_tracker.conf
+echo "mysql_password = $PASSWORD" >> xbt_tracker.conf
+echo "mysql_database = $DATABASE" >> xbt_tracker.conf
+
+xbt_tracker
