82b2b7afaf97077361ceb3cf5c15212f97812d56
Mambix Ltd <ledi.mambix@gmail.com>
2018-12-29 18:00:29 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
+echo "Writing config $USER:$PASSWORD@$HOST:$DATABASE"
 echo "mysql_host = $HOST" > xbt_tracker.conf
 echo "mysql_user = $USER" >> xbt_tracker.conf
 echo "mysql_password = $PASSWORD" >> xbt_tracker.conf
 echo "mysql_database = $DATABASE" >> xbt_tracker.conf
 
-xbt_tracker
+echo "Starting tracker..."
+./xbt_tracker
