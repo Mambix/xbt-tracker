3798784a5b3b6e6b89f09ea65cb35919c31f9af9
Mambix Ltd <ledi.mambix@gmail.com>
2018-12-29 19:59:14 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,5 +4,21 @@ echo "mysql_user = $USER" >> xbt_tracker.conf
 echo "mysql_password = $PASSWORD" >> xbt_tracker.conf
 echo "mysql_database = $DATABASE" >> xbt_tracker.conf
 
+if [ $DEBUG = "true" ]
+  echo "debug = 1" >> xbt_tracker.conf
+fi
+
+if [ $LOG_ACCESS = "true" ]
+  echo "log_access = 1" >> xbt_tracker.conf
+fi
+
+if [ $LOG_ANNOUNCE = "true" ]
+  echo "log_announce = 1" >> xbt_tracker.conf
+fi
+
+if [ $LOG_SCRAPE = "true" ]
+  echo "log_scrape = 1" >> xbt_tracker.conf
+fi
+
 echo "Starting tracker..."
 ./xbt_tracker
